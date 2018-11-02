532f727c62230b2c9f0ad3dacfb54cb15452ea8a
AuthBeam <sapiensarkar@gmail.com>
2018-11-02 07:37:12 +0530

    connection to Postgres and Mongo databases done - 0.1

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 "use strict";
 
-module.exports = function authbeam() {
-  return "AuthBeam!";
+function AuthBeam(dbDriver, dbConn, usersTableName){
+  this.dbDriver = dbDriver;
+  this.dbConn = dbConn;
+  this.usersTableName = usersTableName;
+}
+
+module.exports = AuthBeam;
+
+AuthBeam.prototype.getAllUsers = function () {
+  switch(this.dbDriver.toLowerCase()) {
+    case 'mongodb':
+    case 'mongo-native':
+    case 'mongoose':
+    case 'mongojs':
+      return fetchAllUsersMongo(this.dbConn, this.usersTableName);
+      break;
+    case 'pg':
+    case 'pg-native':
+      return fetchAllUsersPG(this.dbConn, this.usersTableName);
+      break;
+    default:
+      console.log("Your database driver isn't supported. We support: mongodb, mongoose, mongojs, waterfall, pg, pg-native");
+  }
+}
+
+function fetchAllUsersMongo(db, usersTableName) {
+  return new Promise((resolve, reject) => {
+    db.collection(usersTableName).find({}).toArray((err, users) => {
+      if (err) return reject(err);
+      return resolve(users);
+    });
+  });
+}
+
+function fetchAllUsersPG(db, usersTableName) {
+  const query = {
+    text: 'SELECT username FROM ' + usersTableName
+  }
+  return new Promise((resolve, reject) => {
+    db.query(query, (err, res) => {
+      if (err) return reject(err);
+      return resolve (res.rows);
+    });
+  });
 }
